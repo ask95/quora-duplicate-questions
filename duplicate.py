5fd19cf499ff83619e7608cc4642df096b1dd427
Anikesh Kamath <kamathanikesh@gmail.com>
2017-11-20 23:35:26 -0600

    updated model

--- duplicate.py
+++ duplicate.py
@@ -3,6 +3,10 @@
 import sys
 from model import *
 from data_gen import *
+import random
+import numpy as np
+from sklearn.cross_validation import train_test_split
+
 
 
 if __name__ == '__main__':
@@ -12,12 +16,18 @@ if __name__ == '__main__':
     word_vectors = read_word_embeddings("data/glove.6B.300d-relativized.txt")
 
     # Load train, dev, and test exs
-    train_exs = read_and_index(data_path + "train.tsv", word_vectors.word_indexer)
-    #dev_exs = read_and_index("data/dev.txt", word_vectors.word_indexer)
-    test_exs = read_and_index(data_path + "test.tsv", word_vectors.word_indexer)
-    
-    print repr(len(train_exs)) + " / " + repr(len(dev_exs)) + " / " + repr(len(test_exs)) + " train/dev/test examples"
+    exs = read_and_index(data_path + "quora_duplicate_questions.tsv", word_vectors.word_indexer)
+    train_exs, test_exs = train_test_split(exs, test_size=0.3)
+    #df = pd.read_csv('C:/Dataset.csv')
+    #df['split'] = np.random.randn(df.shape[0], 1)
 
+    #dev_exs = read_and_index("data/dev.txt", word_vectors.word_indexer)
+    #test_exs = read_and_index(data_path + "test.tsv", word_vectors.word_indexer)
+
+    print repr(len(train_exs)) + " / " + repr(len(test_exs)) + " train/dev/test examples"
+    test_exs_predicted = train(train_exs, test_exs, word_vectors)
+
+    '''
     if len(sys.argv) >= 2:
         system_to_run = sys.argv[1]
     else:
@@ -43,3 +53,4 @@ if __name__ == '__main__':
         raise Exception("Pass in either FF or FANCY to run the appropriate system")
     # Write the test set output
     write_question_pairs(test_exs_predicted, system_to_run+"_1_test-blind.output.txt", word_vectors.word_indexer)
+    '''
--- model.py
+++ model.py
@@ -3,7 +3,7 @@
 import tensorflow as tf
 import numpy as np
 import random
-from sentiment_data import *
+from data_gen import *
 #from tf.nn.rnn_cell import LSTMCell
 
 
@@ -15,7 +15,221 @@ def pad_to_length(np_arr, length):
     return result
 
 
-def train(train_exs, dev_exs, test_exs, word_embeddings)
+def train(train_exs, test_exs, word_embeddings):
+    print "HEY"
+    # 59 is the max sentence length in the corpus, so let's set this to 60
+    seq_max_len = 237
+    # To get you started off, we'll pad the training input to 237 words to make it a square matrix.
+
+    #TRAINING DATA
+    print "TRAIN Extraction begins!"
+    trainQ1_mat = np.asarray([pad_to_length(np.array(ex.indexed_q1), seq_max_len) for ex in train_exs])
+    trainQ2_mat = np.asarray([pad_to_length(np.array(ex.indexed_q2), seq_max_len) for ex in train_exs])
+    trainQ1_seq_lens = np.array([len(ex.indexed_q1) for ex in train_exs])
+    trainQ2_seq_lens = np.array([len(ex.indexed_q2) for ex in train_exs])
+
+    ## Labels
+    train_labels_arr = np.array([ex.label for ex in train_exs])
+    print train_labels_arr[5]
+
+    ## Matrix with seq word indices and word vectors
+    dim = len(word_embeddings.get_embedding_byidx(0))
+    trainq1_s_input = np.zeros([len(train_exs), seq_max_len, dim])
+    trainq2_s_input = np.zeros([len(train_exs), seq_max_len, dim])
+
+    for i in range(len(trainQ1_mat)):
+        for wi in range(len(trainQ1_mat[i])):
+            word_vec1 = word_embeddings.get_embedding_byidx(trainQ1_mat[i][wi])
+            word_vec2 = word_embeddings.get_embedding_byidx(trainQ2_mat[i][wi])
+
+            trainq1_s_input[i][wi] = word_vec1
+            trainq2_s_input[i][wi] = word_vec2
+
+    print "TRAIN Extraction ends!"
+
+
+
+
+    #TEST DATA
+    print "TEST Extraction begins!"
+    testQ1_mat = np.asarray([pad_to_length(np.array(ex.indexed_q1), seq_max_len) for ex in test_exs])
+    testQ2_mat = np.asarray([pad_to_length(np.array(ex.indexed_q2), seq_max_len) for ex in test_exs])
+    testQ1_seq_lens = np.array([len(ex.indexed_q1) for ex in test_exs])
+    testQ2_seq_lens = np.array([len(ex.indexed_q2) for ex in test_exs])    
+
+    ## Labels
+    test_labels_arr = np.array([ex.label for ex in test_exs])
+
+    ## Matrix with seq word indices and word vectors
+    dim = len(word_embeddings.get_embedding_byidx(0))
+    testq1_s_input = np.zeros([len(test_exs), seq_max_len, dim])
+    testq2_s_input = np.zeros([len(test_exs), seq_max_len, dim])
+
+    for i in range(len(testQ1_mat)):
+        for wi in range(len(trainQ1_mat[i])):
+            word_vec1 = word_embeddings.get_embedding_byidx(testQ1_mat[i][wi])
+            word_vec2 = word_embeddings.get_embedding_byidx(testQ2_mat[i][wi])
+
+            testq1_s_input[i][wi] = word_vec1
+            testq2_s_input[i][wi] = word_vec2
+
+    print "TEST Extraction ends!"
+
+
+
+    
+    #serial input (seq of 300 corresponding to each word)
+    
+    batch_size = 1
+    feat_vec_size = dim
+    #embedding_size = feat_vec_size
+
+    # We're using 2 classes. What's presented here is multi-class code that can scale to more classes, though
+    # slightly more compact code for the binary case is possible.
+    num_classes = 2
+
+    # DEFINING THE COMPUTATION GRAPH
+
+    q1 = tf.placeholder(tf.float32, [batch_size, seq_max_len, dim])
+    q2 = tf.placeholder(tf.float32, [batch_size, seq_max_len, dim])
+    label = tf.placeholder(tf.int32, 1)
+    q1_len = tf.placeholder(tf.int32, 1)
+    q2_len = tf.placeholder(tf.int32, 1)
+
+    num_cells = 100
+
+    def myLSTMcell():
+        lstm = tf.nn.rnn_cell.LSTMCell(num_cells, reuse=tf.get_variable_scope().reuse)
+        return lstm
+
+    myLSTMcell1 = myLSTMcell()
+    myLSTMcell2 = myLSTMcell()
+
+    initialS = myLSTMcell.zero_state(1, tf.float32)
+    #sent_input = tf.unstack(sent)
+    sent_input = sent
+    output1, _ = tf.nn.dynamic_rnn(myLSTMcell1, sent_input, dtype=tf.float32) #sequence_length=sent_len, dtype=tf.float32)
+    output2, _ = tf.nn.dynamic_rnn(myLSTMcell2, sent_input, dtype=tf.float32) #sequence_length=sent_len, dtype=tf.float32)
+    #print "AKAMATH", output.shape, type(output)
+    #z = output[0][-1]
+
+    #on the basis of conclusions from last assignment, we use the mean vector instead of the last vector
+    z1 = tf.reduce_mean(output1[0], axis=0)
+    z2 = tf.reduce_mean(output2[0], axis=0)
+    
+    #combining the LSTM representation of the 2 questions
+    z = tf.concat([z1, z2], 0)
+
+    #print "Z's shape is ", z.shape
+    #print "Hey!", output.shape
+    W = tf.get_variable("W", [num_classes, num_cells], 
+        initializer=tf.contrib.layers.xavier_initializer(seed=0))
+    print "HI SEXY", W.shape, z.shape
+    #b = tf.get_variable("b", [num_classes])
+    #probs = tf.contrib.layers.fully_connected([output[0][-1]], num_classes, activation_fn=tf.nn.softmax)
+    #print probs.shape
+    #probs = tf.reshape(probs, shape=[1,num_classes])
+    probs = tf.nn.softmax(tf.tensordot(W, z, 1))
+    one_best = tf.argmax(probs)
+
+    label_onehot = tf.reshape(tf.one_hot(label, num_classes), shape=[num_classes])
+    print probs.shape, label_onehot.shape
+    
+
+    loss = tf.negative(tf.log(tf.tensordot(probs, label_onehot, 1)))
+
+
+    decay_steps = 10
+    learning_rate_decay_factor = 0.99995
+    global_step = tf.contrib.framework.get_or_create_global_step()
+    # Smaller learning rates are sometimes necessary for larger networks
+    initial_learning_rate = 0.001
+    # Decay the learning rate exponentially based on the number of steps.
+    lr = tf.train.exponential_decay(initial_learning_rate,
+                                    global_step,
+                                    decay_steps,
+                                    learning_rate_decay_factor,
+                                    staircase=True)
+    # Logging with Tensorboard
+    tf.summary.scalar('learning_rate', lr)
+    tf.summary.scalar('loss', loss)
+    # Plug in any first-order method here! We'll use Adam, which works pretty well, but SGD with momentum, Adadelta,
+    # and lots of other methods work well too
+    #opt = tf.train.AdamOptimizer(lr)
+    opt = tf.train.AdamOptimizer()
+    # Loss is the thing that we're optimizing
+    grads = opt.compute_gradients(loss)
+    # Now that we have gradients, we operationalize them by defining an operator that actually applies them.
+    apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+    with tf.control_dependencies([apply_gradient_op]):
+        train_op = tf.no_op(name='train')
+
+    # RUN TRAINING AND TEST
+    # Initializer; we need to run this first to initialize variables
+    init = tf.global_variables_initializer()
+    num_epochs = 10
+    merged = tf.summary.merge_all()  # merge all the tensorboard variables
+    # The computation graph must be run in a particular Tensorflow "session". Parameters, etc. are localized to the
+    # session (unless you pass them around outside it). All runs of a computation graph with certain values are relative
+    # to a particular session
+    with tf.Session() as sess:
+        # Write a logfile to the logs/ directory, can use Tensorboard to view this
+        train_writer = tf.summary.FileWriter('logs/', sess.graph)
+        # Generally want to determinize training as much as possible
+        tf.set_random_seed(0)
+        # Initialize variables
+        sess.run(init)
+        step_idx = 0
+        for i in range(0, num_epochs):
+            print "Epoch:", i
+            loss_this_iter = 0
+            # batch_size of 1 here; if we want bigger batches, we need to build our network appropriately
+            for ex_idx in xrange(0, len(trainq1_s_input)):
+                # sess.run generally evaluates variables in the computation graph given inputs. "Evaluating" train_op
+                # causes training to happen
+                # [_, loss_this_instance, summary] = sess.run([train_op, loss, merged], feed_dict = {fx: train_xs[ex_idx],
+                #                                                                   label: np.array([train_ys[ex_idx]])})
+                [_, loss_this_instance, summary] = sess.run([train_op, loss, merged], feed_dict = {q1: [trainq1_s_input[ex_idx]],
+                                                                                    q2: [trainq2_s_input[ex_idx]],
+                                                                                   label: np.array([train_labels_arr[ex_idx]]),
+                                                                                   q2_len: np.array([trainQ2_seq_lens[ex_idx]]), 
+                                                                                   q1_len: np.array([trainQ1_seq_lens[ex_idx]])})
+                step_idx += 1
+                loss_this_iter += loss_this_instance
+            print "Loss for iteration " + repr(i) + ": " + repr(loss_this_iter)
+        
+        # Evaluate on the test set
+            dev_correct = 0
+            for ex_idx in xrange(0, len(testq1_s_input)):
+                # Note that we only feed in the x, not the y, since we're not training. We're also extracting different
+                # quantities from the running of the computation graph, namely the probabilities, prediction, and z
+                [probs_this_instance, pred_this_instance] = sess.run([probs, one_best],
+                                                                                  feed_dict={q1: [testq1_s_input[ex_idx]],
+                                                                                    q2: [testq2_s_input[ex_idx]],
+                                                                                   label: np.array([test_labels_arr[ex_idx]]),
+                                                                                   q2_len: np.array([testQ2_seq_lens[ex_idx]]), 
+                                                                                   q1_len: np.array([testQ1_seq_lens[ex_idx]])})
+                if (dev_labels_arr[ex_idx] == pred_this_instance):
+                    dev_correct += 1
+            # print "Example " + repr(test_serial_input[ex_idx]) + "; gold = " + repr(test_labels_arr[ex_idx]) + "; pred = " +\
+            #       repr(pred_this_instance) + " with probs " + repr(probs_this_instance)
+            # print "  Hidden layer activations for this example: " + repr(z_this_instance)
+            print repr(dev_correct) + "/" + repr(len(dev_labels_arr)) + " correct after training"
+            print 1.0*dev_correct/len(dev_labels_arr)
+ 
+'''           
+        for ex_idx in xrange(0, len(test_serial_input)):
+            
+                # Note that we only feed in the x, not the y, since we're not training. We're also extracting different
+                # quantities from the running of the computation graph, namely the probabilities, prediction, and z
+            [probs_this_instance, pred_this_instance, z_this_instance] = sess.run([probs, one_best, z],
+                                                                                  feed_dict={sent: [test_serial_input[ex_idx]]})
+            
+            test_exs[ex_idx].label = pred_this_instance
+        
+        return test_exs
+'''
+
 
 
 # Train a feedforward neural network on the given training examples, using dev_exs for development and returning
