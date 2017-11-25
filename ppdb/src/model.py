2b3d34060a25a63581a0aa113d8f948b4ced64b1
prasoongoyal <prasoongoyal13@gmail.com>
2017-11-24 18:59:03 -0600

    LSTM codefor PPDB (untested)

---
+++ ppdb/src/model.py
@@ -0,0 +1,120 @@
+import numpy as np
+from data_gen import *
+from sklearn.linear_model import LogisticRegression
+import tensorflow as tf
+
+def generate_avg_embedding(indexed_q, word_vectors):
+    dim = len(word_vectors.get_embedding_byidx(0))
+    emb = np.zeros(dim)
+    for word_idx in indexed_q:
+        emb += word_vectors.get_embedding_byidx(word_idx)
+    return emb/len(indexed_q)
+
+class AvgEmbeddingModel:
+    def __init__(self, model, word_vectors):
+        self.model = model
+        self.word_vectors = word_vectors
+
+    def predict(self, test_exs):
+        X = []
+        y = []
+        # generate X's for test examples
+        for ex in test_exs:
+            q1_avg_emb = generate_avg_embedding(ex.indexed_q1, self.word_vectors)
+            q2_avg_emb = generate_avg_embedding(ex.indexed_q1, self.word_vectors)
+            X.append(q1_avg_emb * q2_avg_emb)
+            y.append(ex.label)
+        print 'Test accuracy:', self.model.score(X, y)
+           
+
+def train_avg_model(train_exs, word_vectors):
+    # generate average embeddings
+    X = []
+    y = []
+    for ex in train_exs:
+        q1_avg_emb = generate_avg_embedding(ex.indexed_q1, word_vectors)
+        q2_avg_emb = generate_avg_embedding(ex.indexed_q1, word_vectors)
+        X.append(q1_avg_emb * q2_avg_emb)
+        y.append(ex.label)
+
+    model = LogisticRegression()
+    model = model.fit(X, y)
+    print 'Training accuracy:', model.score(X, y)
+    return AvgEmbeddingModel(model, word_vectors)
+
+def train_lstm_model(train_exs, word_vectors, ppdb_pairs):
+    n_classes = 2
+    lstm_size = 100
+    seq_max_len = 300
+    dim = len(word_vectors.get_embedding_byidx(0))
+
+    #defining the computation graph
+    q1 = tf.placeholder(tf.float32, [None, seq_max_len, dim])
+    len1 = tf.placeholder(tf.int32, 1)
+    q2 = tf.placeholder(tf.float32, [None, seq_max_len, dim])
+    len2 = tf.placeholder(tf.int32, 1)
+
+    def myLSTMCell(reuse_):
+        lstm = tf.nn.rnn_cell.LSTMCell(lstm_size, reuse=reuse_)
+        return lstm
+
+    #lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
+
+    x1 = tf.unstack(q1, seq_max_len, 1)
+    x2 = tf.unstack(q2, seq_max_len, 1)
+    outputs1, states1 = tf.contrib.rnn.static_rnn(myLSTMCell(tf.get_variable_scope().reuse), x1, sequence_length=len1, dtype=tf.float32)
+    outputs2, states2 = tf.contrib.rnn.static_rnn(myLSTMCell(True), x2, sequence_length=len2, dtype=tf.float32)
+
+    states_concat = tf.concat((tf.squeeze(states1[-1]), tf.squeeze(states2[-1])), axis=-1)
+
+    W = tf.get_variable("W", [n_classes, 2 * lstm_size])
+    probs = tf.tensordot(W, states_concat, 1)
+
+    prediction = tf.argmax(probs)
+
+    label = tf.placeholder(tf.int32, 1)
+    label_onehot = tf.reshape(tf.one_hot(label, n_classes), shape=[n_classes])
+
+    loss = tf.losses.softmax_cross_entropy(label_onehot, probs)
+
+    # training algorithm parameters
+    decay_steps = 1000
+    learning_rate_decay_factor = 0.99
+    global_step = tf.contrib.framework.get_or_create_global_step()
+    initial_learning_rate = 0.01
+    lr = tf.train.exponential_decay(initial_learning_rate,
+                                    global_step,
+                                    decay_steps,
+                                    learning_rate_decay_factor,
+                                    staircase=True)
+    tf.summary.scalar('learning_rate', lr)
+    tf.summary.scalar('loss', loss)
+    opt = tf.train.AdamOptimizer(lr)
+    grads = opt.compute_gradients(loss)
+    apply_gradients_op = opt.apply_gradients(grads, global_step=global_step)
+    with tf.control_dependencies([apply_gradients_op]):
+        train_op = tf.no_op(name='train')
+
+    init = tf.global_variables_initializer()
+    n_epochs = 10
+    merged = tf.summary.merge_all()
+
+    with tf.Session() as sess:
+        train_writer = tf.summary.FileWriter('../logs/', sess.graph)
+        tf.set_random_seed(0)
+        sess.run(init)
+        step_idx = 0
+        for i in range(0, n_epochs):
+            print 'Epoch:', i
+            loss_this_iter = 0
+
+            for ex_idx in xrange(0, len(train_exs)):
+                [_, loss_this_instance, summary] = sess.run([train_op, loss, merged], feed_dict = {
+                    q1: [map(word_vectors.get_embedding_byidx, train_exs[ex_idx].indexed_q1)], 
+                    q2: [map(word_vectors.get_embedding_byidx, train_exs[ex_idx].indexed_q2)], 
+                    label: np.array([train_exs[ex_idx].label]), 
+                    len1: len(train_exs[ex_idx].indexed_q1),
+                    len2: len(train_exs[ex_idx],indexed_q2)})
+                step_idx += 1
+                loss_this_iter += loss_this_instance
+            print 'Loss for iteration ' + repr(i) + ': ' + repr(loss_this_iter)
--- ppdb/src/ppdb-pred.py
+++ ppdb/src/ppdb-pred.py
@@ -1,8 +1,24 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-question_pairs_file = sys.argv[1]
-embeddings_file = sys.argv[2]
+from data_gen import *
+from sklearn.cross_validation import train_test_split
+from model import *
+#question_pairs_file = sys.argv[1]
+#embeddings_file = sys.argv[2]
+
+word_vectors = read_word_embeddings('../data/paragram-phrase-XXL.txt')
+#exs = read_and_index('../data/q10k.txt', word_vectors.word_indexer)
+exs = read_and_index('../data/quora_duplicate_questions.tsv', word_vectors.word_indexer)
+#print len(exs)
+#raw_input()
+
+train_exs, test_exs = train_test_split(exs, test_size=0.3)
+
+#avg_model = train_avg_model(train_exs, word_vectors)
+#test_exs_predicted = avg_model.predict(test_exs)
+
+lstm_model = train_lstm_model(train_exs, word_vectors, None)
 
 # load embeddings
 word_emb = {}
@@ -13,6 +29,8 @@ for line in open(embeddings_file):
 
 label0_pred = []
 label1_pred = []
+
+
 
 for line in open(question_pairs_file):
   line = line.strip()
