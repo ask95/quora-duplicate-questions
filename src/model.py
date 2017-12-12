e291223fb6bddff5ed3d78657539be4585930f02
Anikesh Kamath <kamathanikesh@gmail.com>
2017-12-12 01:14:39 -0600

    too bored to make cool message commits

--- src/model.py
+++ src/model.py
@@ -2077,7 +2077,7 @@ def train_bench9(train_exs, test_exs, word_embeddings, initial_learning_rate = 0
     # print sent1_f
    # sent2_f = tf.tensordot(output2, F, 1)
 
-    att = tf.tensordot(sent1_f, tf.transpose(sent2_f), 1)
+    att = tf.tensordot(sent1_f, sent2_f, 1)
     #att = tf.transpose(att)
     exp_att = tf.exp(att)
     print exp_att.get_shape() 
